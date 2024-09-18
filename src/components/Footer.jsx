af08b5d024c1e05f67f6d28948fd2d0cfc1e820d
Matheus da Silva <contagodzillajapan@gmail.com>
2024-09-18 04:26:06 -0300

    Feat: desenvolvendo footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <footer className="flex flex-col items-center justify-center bg-black text-[#F29C6B] p-4 text-center shadow-xl">
+      <p>&copy; 2024 Legendary. Monsterverse TM - Desenvolvido por <a href="https://github.com/MatheusGODZILLA" target="_blank" className="hover:text-white">Matheus da Silva</a></p>
+    </footer>
+);
+
+export default Footer;

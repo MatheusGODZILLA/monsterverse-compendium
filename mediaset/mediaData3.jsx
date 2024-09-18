f261ad8294f95943464cc69d02d2c61891c4112f
Matheus da Silva <contagodzillajapan@gmail.com>
2024-09-18 04:00:04 -0300

    Feat: criando datasets e subindo imagens

---
+++ mediaset/mediaData3.jsx
@@ -0,0 +1,4 @@
+export const mediaData3 = [
+    { imageSrc: '/img/media/Godzilla_Smash3_Icon.png', title: 'Godzilla: Smash3', description: 'Descrição do Godzilla: Smash3' },
+    { imageSrc: '/img/media/6b8cda7c5814b2b3dff27af566afd12a.jpg', title: 'Godzilla x Kong: Titan Chasers', description: 'Descrição do Godzilla x Kong: Titan Chasers' },
+];
